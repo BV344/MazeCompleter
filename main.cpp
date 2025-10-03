a9f262751b9c76c46b2f22ea8719b7d520e5bf28
bvizcaino <bvizcaino@puc.edu>
2025-10-03 15:16:45 -0700

    CMakeLists.txt File

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 #include "CImg.h"
 
 using namespace cimg_library;
+using namespace std;
+
+void breadthFirstSearch(CImg<unsigned char> &inputImg) {
+    int x = inputImg.width();
+    int y = inputImg.height();
+
+    cout << "X: " << x << " Y: " << y << endl;
+
+    for (int i = 0; i < x/2; i++) {
+        for (int j = 0; j < y/2; j++) {
+            inputImg(i,j,0,0) = 255;
+
+
+        }
+    }
+}
+
+void bestFirstSearch(CImg<unsigned char> &inputImg) {}
+
+
 
 int main() {
     CImg<unsigned char> img("input.bmp");
@@ -14,11 +34,13 @@ int main() {
     CImg<unsigned char> rgbGray = gray.get_normalize(0,255).resize(-100,-100,1,3);
 
     //Saving for Debugging Purposes
-    rgbGray.save("output_grayscale.bmp");
+    // rgbGray.save("output_grayscale.bmp");
 
     img = rgbGray;
 
     img(100,100,0,0) = 255;
+
+    breadthFirstSearch(img);
 
 
     img.display("RGBGRAY");
